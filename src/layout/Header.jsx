0bfa82652c3ea9dd4d336386d5a2691eb288fc3d
haneeoh <honey9473@gmail.com>
2025-05-22 17:33:17 +0900

    main->login 이동 페이지, login 페이지 css 수정,main페이지 수정중

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,7 +1,7 @@
 import "./Header.css"
 import "../style.css"
-import { LuMoveUpRight} from "react-icons/lu";
-import { useNavigate } from "react-router-dom";
+import {LuMoveUpRight} from "react-icons/lu";
+import {useNavigate} from "react-router-dom";
 
 export default function Header() {
     const navigate = useNavigate();
@@ -10,18 +10,19 @@ export default function Header() {
         <header className={"header"}>
             <div className="header-container">
                 <div className="logo"
-                onClick={()=>navigate("/")}>*Bitground</div>
+                     onClick={() => navigate("/")}>*Bitground
+                </div>
                 <div className={"nav-container"}>
                     <div className={"nav-frame"}>
-                    <div className={"nav-list"}>
-                        <div className={"nav-sell"}>거래소</div>
-                        <div className={"nav-selllist"}>투자내역</div>
-                        <div className={"nav-news"}>최신동향</div>
-                        <div className={"nav-community"}>커뮤니티</div>
-                        <div className={"nav-rank"}>랭킹</div>
-                        <div className={"nav-service"}>고객센터</div>
+                        <div className={"nav-list"}>
+                            <div className={"nav-sell"}>거래소</div>
+                            <div className={"nav-selllist"}>투자내역</div>
+                            <div className={"nav-news"}>최신동향</div>
+                            <div className={"nav-community"}>커뮤니티</div>
+                            <div className={"nav-rank"}>랭킹</div>
+                            <div className={"nav-service"}>고객센터</div>
+                        </div>
                     </div>
-                </div>
                 </div>
                 <div className="header-btns">
                     <button type={"button"} className="login-btn"
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,8 +1,16 @@
+import './Main.css';
 
 export default function Main() {
     return (
         <div className="main-container">
-            안ㅇㅇㅇㅇ
+            <div className={"banner-container"}>
+                <div className={"text-banner"}>Invest everything</div>
+                <div className={"banner-image"}>
+                    <div className={"ipad"}>
+                        <div className={"ipad-screen"}></div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
