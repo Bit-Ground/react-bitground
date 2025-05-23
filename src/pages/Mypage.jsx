8f3076cf8c51cecb0ea7baa3242634f38dbd25fa
haneeoh <honey9473@gmail.com>
2025-05-23 17:24:10 +0900

    trade css 수정

--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import './Mypage.css';
 
-export default function Mypage(){
+export default function Mypage() {
 
     return (
         <div className="mypage-container">
             <div className={"mypage-title"}>
                 * 마이페이지
+            </div>
+            <div className={"mypage-content"}>
+                <div className={"mypage-list"}>
+                    <div className={"invest-info"}>
+                        투자정보
+                    </div>
+                    <div className={"my-info"}>
+                        개인정보 수정
+                    </div>
+                    <div className={"invest-last"}>
+                        지난시즌 내역
+                    </div>
+                </div>
+                <div className={"content-list"}>
+
+                </div>
             </div>
         </div>
     );
