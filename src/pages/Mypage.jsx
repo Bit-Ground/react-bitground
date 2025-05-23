dfff7426c39714d988ab3791af25b43f8946ed3e
haneeoh <honey9473@gmail.com>
2025-05-23 16:16:37 +0900

    header 해당 페이지시 색 유지 마이페이지 추가

---
+++ src/pages/Mypage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Mypage.css';
+
+export default function Mypage(){
+
+    return (
+        <div className="mypage-container">
+            <div className={"mypage-title"}>
+                * 마이페이지
+            </div>
+        </div>
+    );
+}
