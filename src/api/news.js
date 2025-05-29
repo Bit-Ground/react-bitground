b340d58471e6ba2b2ee03af8455fd22ecbc68d2a
Macro4760 <seho4760@gmail.com>
2025-05-29 18:38:33 +0900

    gemini 분석 추가

--- src/api/news.js
+++ src/api/news.js
@@ -1,8 +1,10 @@
+// src/api/news.js
+
 import axios from 'axios';
 
 const BASE_URL = '/api/news';
 
-export const fetchNews = async (keyword, start = 1, display = 10) => {
+export const fetchNews = async (keyword, start = 1, display = 5) => { // display 기본값을 4로 변경
     try {
         const response = await axios.get(BASE_URL, {
             params: { keyword, start, display },
