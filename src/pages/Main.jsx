3073174291f5cfaaa1f136c73beb639a4eb89a0e
haneeoh <honey9473@gmail.com>
2025-06-02 14:02:32 +0900

    gbchart 진행중

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,6 +1,10 @@
 import '../styles/Main.css';
 
+import GbIndexChart from "../components/main/GbIndexChart.jsx";
+
+
 export default function Main() {
+
     return (
         <div className="main-container">
             <div className={"banner-container"}>
@@ -14,7 +18,10 @@ export default function Main() {
                 </div>
             </div>
             <div className={"market-price-container"}>
-
+                <h1>📊 업비트 시장 지수 대시보드</h1>
+                <GbIndexChart  />
+                {/*<GbmiChart />*/}
+                {/*<GbaiChart />*/}
             </div>
             <div className={"rank-container"}>
 
