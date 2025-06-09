406edf542380709ace04d8c5c572ae5397cfddfe
leejaeho-17 <woghdl99@naver.com>
2025-06-09 17:46:14 +0900

    0609 오후 service ask, notice 프론트 작업

--- src/components/service/Ask.jsx
+++ src/components/service/Ask.jsx
@@ -1,9 +1,59 @@
 import React from 'react';
+import { RiDeleteBinLine } from "react-icons/ri";
+import { LuPencilLine } from "react-icons/lu";
+
 
 const Ask = () => {
     return (
         <div>
-            ask
+            <div>
+                <div>
+                    <div className='ask-list'>
+                        <table className='ask-table'>
+                            <colgroup>
+                                <col style={{ width: '5%' }} />
+                                <col style={{ width: '60%' }} />
+                                <col style={{ width: '10%' }} />
+                                <col style={{ width: '15%' }} />
+                                <col style={{ width: '10%' }} />
+                            </colgroup>
+                            <thead>
+                            <tr className='ask-table-head'>
+                                <th>번호</th>
+                                <th>제목</th>
+                                <th></th>
+                                <th>작성자</th>
+                                <th>등록일</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr className='ask-row'>
+                                <td>1</td>
+                                <td className='ask-title'>
+                                    test
+                                </td>
+                                <td><LuPencilLine className='writeicon'/>
+                                    <RiDeleteBinLine className='delicon'/></td>
+                                <td>작성자</td>
+                                <td>0609</td>
+                            </tr>
+                            </tbody>
+                            <tfoot>
+                            <tr>
+                                <td colSpan="4">
+                                    <button className='ask-pagination'>
+                                        &lt; 이전
+                                    </button>
+                                    <button className='ask-pagination'>
+                                        다음 &gt;
+                                    </button>
+                                </td>
+                            </tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/service/Notice.jsx
+++ src/components/service/Notice.jsx
@@ -3,12 +3,50 @@ import React from 'react';
 
 const Notice = () => {
 
-
-
-
     return (
         <div>
-
+            <div>
+                <div className='notice-list'>
+                    <table className='notice-table'>
+                        <colgroup>
+                            <col style={{ width: '5%' }} />
+                            <col style={{ width: '70%' }} />
+                            <col style={{ width: '15%' }} />
+                            <col style={{ width: '10%' }} />
+                        </colgroup>
+                        <thead>
+                        <tr className='notice-table-head'>
+                            <th>번호</th>
+                            <th>제목</th>
+                            <th>작성자</th>
+                            <th>등록일</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            <tr className='notice-row'>
+                                <td>1</td>
+                                <td className='notice-title'>
+                                    test
+                                </td>
+                                <td>관리자</td>
+                                <td>0609</td>
+                            </tr>
+                        </tbody>
+                        <tfoot>
+                        <tr>
+                            <td colSpan="4">
+                                <button className='notice-pagination'>
+                                    &lt; 이전
+                                </button>
+                                <button className='notice-pagination'>
+                                    다음 &gt;
+                                </button>
+                            </td>
+                        </tr>
+                        </tfoot>
+                    </table>
+                </div>
+            </div>
         </div>
     );
 };
