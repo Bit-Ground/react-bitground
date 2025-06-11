e42a9b5ca43300111d2e68be68ebdc172c4e1aa4
lepe99 <kr_@kakao.com>
2025-06-11 14:57:22 +0900

    랭킹페이지 디자인 1차 수정

--- src/components/rank/PastRankingList.jsx
+++ src/components/rank/PastRankingList.jsx
@@ -5,25 +5,25 @@ import React from "react";
 
 export default function PastRankingList({seasons, pastRankingsMap, pastLoading, selectedSeason, setSelectedSeason}) {
     return (
-
-            <div className="past-ranking-wrapper">
-                <div className="section-header">
+        <div className="past-ranking-wrapper">
+            <div className="section-header">
+                <div className="title-and-date-group">
                     <span className="section-title">지난시즌 랭킹</span>
-                    <select className="season-select" value={selectedSeason}
-                            onChange={(e) => setSelectedSeason(Number(e.target.value))}>
-                        {seasons.map(season => (
-                            <option key={season.id} value={season.id}>
-                                {season.name}
-                            </option>
-                        ))}
-                    </select>
                     <span className="season-date">
                            {seasons.find(s => s.id === selectedSeason)?.startAt?.substring(0, 10)} ~ {seasons.find(s => s.id === selectedSeason)?.endAt?.substring(0, 10)}
-                       </span>
+                        </span>
                 </div>
-                {pastLoading ? <LoadingSpinner/> :
-                    <RankingList data={pastRankingsMap[selectedSeason] || []} highlightTop3/>}
-
+                <select className="season-select" value={selectedSeason}
+                        onChange={(e) => setSelectedSeason(Number(e.target.value))}>
+                    {seasons.map(season => (
+                        <option key={season.id} value={season.id}>
+                            {season.name}
+                        </option>
+                    ))}
+                </select>
             </div>
+            {pastLoading ? <LoadingSpinner/> :
+                <RankingList data={pastRankingsMap[selectedSeason] || []} highlightTop3/>}
+        </div>
     );
 }
