4f245fc74edfbf366099468012ccb4bc56174ff9
leejaeho-17 <woghdl99@naver.com>
2025-06-16 15:53:25 +0900

    0616 오후 문의사항 열람제한 기능 구현

--- src/components/service/Ask.jsx
+++ src/components/service/Ask.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import api from '../../api/axiosConfig';
 import { RiDeleteBinLine } from "react-icons/ri";
-import { BiSubdirectoryRight } from "react-icons/bi";
 import { LuPencilLine } from "react-icons/lu";
 import "../../styles/service/service.css";
 import { useAuth } from '../../auth/useAuth.js';
